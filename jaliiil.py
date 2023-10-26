08ad1478a5004cfa1cb3beb33157bcde41a2e3e5
jalilha <121061854+jalilha@users.noreply.github.com>
2023-10-26 17:57:21 +0100

    Update jaliiil.py

--- jaliiil.py
+++ jaliiil.py
@@ -233,7 +233,7 @@ def menu():
                                 linex()
                                 plist = []
                                 try:
- o                                     ps_limit = int(input(' How many passwords do you want to add ? '))
+                                        ps_limit = int(input('How many passwords do you want to add ?'))
                                 except:
                                         ps_limit =1
                                 linex()
