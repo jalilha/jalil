4d2b4861ed9d8ae5de1c208efca2c136a813d903
jalilha <121061854+jalilha@users.noreply.github.com>
2023-10-27 20:02:59 +0100

    Update jaliiil.py

--- jaliiil.py
+++ jaliiil.py
@@ -44,6 +44,25 @@ for agent in range(10000):
         l='Mobile Safari/533.1'
         fullagnt=(f'{aa} {b}; {c}{d}{e}{f}) {g}{h}.{i}.{j}.{k} {l}')
         ugen.append(fullagnt)
+        ##______COLORS____ARE________######
+pwx=[]
+W = '\033[97;1m'
+R = '\033[91;1m'
+G = '\033[92;1m'
+Y = '\033[93;1m'
+B = '\033[94;1m'
+P = '\033[95;1m'
+S = '\033[96;1m'
+N = '\x1b[0m'
+PURPLE ='\x1b[38;5;46m'
+RED = '\033[1;91m'
+WHITE = '\033[1;97m'
+GREEN = '\033[1;32m'
+YELLOW = '\033[1;33m'
+BLUE = '\033[1;34m'
+ORANGE = '\033[1;35m'
+BLACK="\033[1;30m"
+EXTRA ='\x1b[38;5;208m'   
 #________________________________________#
 
 logo=("""
