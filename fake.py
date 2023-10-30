6c082f4373549077a9c0e0cf62bb68e0537a3d06
jalilha <121061854+jalilha@users.noreply.github.com>
2023-10-31 00:26:25 +0100

    Update fake.py

--- fake.py
+++ fake.py
@@ -4,16 +4,16 @@ P = '\033[1;35m'
 C = '\033[1;36m'
 W = '\033[1;37m'
 logo=(f"""
-{W}     oooo       .o.       ooooo        ooooo ooooo        
+    oooo       .o.       ooooo        ooooo ooooo        
    `888      .888.      `888'        `888' `888'        
     888     .8"888.      888          888   888         
     888    .8' `888.     888          888   888         
     888   .88ooo8888.    888          888   888         
     888  .8'     `888.   888       o  888   888       o 
 .o. 88P o88o     o8888o o888ooooood8 o888o o888ooooood8 
-`Y888P    {GREEN}A {RED}L {WHITE}G                              
+`Y888P                                  
 {W} ================================================
- [•] Admin      : Jalil XD
+ [•] Admin      : JALIL XD
  [•] GitHub     : JALILHA
 {W} ================================================
 {W} [•]{G} NOTE{W}       :{G} REAL KEY TO FAKE KEY GENERATOR 
