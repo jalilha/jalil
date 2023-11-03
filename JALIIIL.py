9aee20843f045b5f907f64278a1419824062b0a2
jalilha <121061854+jalilha@users.noreply.github.com>
2023-11-03 17:58:56 +0100

    Update JALIIIL.py

--- JALIIIL.py
+++ JALIIIL.py
@@ -202,9 +202,9 @@ def menu():
                                 linex()
                                 file=input('Put file path\033[1;37m:')
                                 try:
-                                        fo=open(file,'r').read().splitlines()
-                                except FileNotFoundError:
-                                        print(' File location not found ')
+					file=open(file,'r').read().splitlines()
+				except FileNotFoundError:
+                                        #print(' File location not found ')
                                         time.sleep(1)
                                         menu()
                                 clear()
